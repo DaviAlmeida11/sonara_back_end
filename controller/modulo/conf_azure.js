858662c787a866fbf0b0e9d3322a8caf4cf039bc
DaviAlmeida11 <davi.almeida1713@gmail.com>
2026-05-16 21:59:45 -0300

    finalizacao do upload de imagens na tabela foto

--- controller/modulo/conf_azure.js
+++ controller/modulo/conf_azure.js
@@ -1,5 +1,5 @@
-const TOKEN = 'sp=r&st=2026-05-08T16:47:56Z&se=2026-06-23T01:02:56Z&sv=2025-11-05&sr=c&sig=pIF9yslJspyY1u9SUGx6qRuXN6sERE971D6mpAb9sT0%3D'
-const ACCOUNT = 'tccsonara'// colocar nome  lá na azure 
+const TOKEN = 'sp=racwdli&st=2026-05-17T00:41:10Z&se=2026-05-25T08:56:10Z&sv=2025-11-05&sr=c&sig=yzO8vtrG9nWxTng8SdToPrRhJisY0IzgIdiM1XZM4ps%3D'
+const ACCOUNT = 'uploadsonara'// colocar nome  lá na azure 
 const CONTAINER = 'uploadsonara' // colocar nome  lá na azure 
 
 
