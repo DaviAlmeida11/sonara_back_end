c36a1a09c8aee1cdb714b24a15203cc712671cec
DAVI DE ALMEIDA SANTOS <25132494@CFP127ALUNO.LOCAL>
2026-05-19 13:42:06 -0300

    configuracao do get de usuario atualizada

--- model/DAO/usuario.js
+++ model/DAO/usuario.js
@@ -15,7 +15,7 @@ const knexDatabase = knex(knexConfig.development);
 const getSelectAllUsers = async function(){
     try {
       
-        let sql = `select * from tb_usuario order by id_usuario desc`
+        let sql = `select * from vw_usuario`
     
         let result = await knexDatabase.raw(sql)
 
